e5bd57c1e7cc245fcc1fb5a274618bf283f1336f
lavsexpert <lavrov-sergey@yandex.ru>
2020-10-03 09:45:07 +0300

    Добавлен запрос на список пользователей к серверу

--- app/src/main/java/club/plus1/client/ApiInterface.java
+++ app/src/main/java/club/plus1/client/ApiInterface.java
@@ -1,11 +1,17 @@
 package club.plus1.client;
 
+import java.util.List;
+
 import io.reactivex.Observable;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 public interface ApiInterface {
 
     @GET("users/{user}")
     Observable<GitHubUserDetail> user(@Path("user") String user);
+
+    @GET("search/users")
+    Observable<List<GitHubUser>> search(@Query("q") String q);
 }
--- app/src/main/java/club/plus1/client/ListActivity.java
+++ app/src/main/java/club/plus1/client/ListActivity.java
@@ -5,15 +5,25 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.schedulers.Schedulers;
+
 public class ListActivity extends AppCompatActivity {
 
+    EditText textSearch;
     RecyclerView recyclerView;
     ListAdapter adapter;
     List<GitHubUser> list;
+    ApiInterface api;
+    private CompositeDisposable disposables;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +34,28 @@ public class ListActivity extends AppCompatActivity {
         recyclerView = findViewById(R.id.list);
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        textSearch = findViewById(R.id.textSearch);
+        api = ApiConfiguration.getApi();
+        disposables = new CompositeDisposable();
+    }
+
+    public void onClick(View view){
+        disposables.add(api.search(textSearch.getText().toString())
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe((users) -> {
+                    list.clear();
+                    list.addAll(users);
+                    adapter.notifyDataSetChanged();
+                }, (error) -> Toast.makeText(this, "При поиске возникла ошибка:\n"+error.getMessage(),
+                        Toast.LENGTH_LONG).show()));
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (disposables.isDisposed()) {
+            disposables.dispose();
+        }
     }
 }
